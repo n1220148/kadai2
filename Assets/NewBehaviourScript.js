72bac4a228b62217e0de88991cd6ccc586654590
n1220148 <bcmk1220148@gn.iwasaki.ac.jp>
2013-01-24 11:15:14 +0900

    kadai2

---
+++ Assets/NewBehaviourScript.js
@@ -0,0 +1,10 @@
+#pragma strict
+
+function Start () { // 開始時は重力影響なし
+this.rigidbody.useGravity = false;
+}
+function Update () { // マウスクリックで重力開始
+if (Input.GetButtonDown("Fire1")) {
+this.rigidbody.useGravity = true;
+}
+}
